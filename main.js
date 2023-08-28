3c7154feb1fc7f25c1ef8f54d675738a5b3f0837
amirsalimov <salimovamirxon27@gmail.com>
2023-08-28 00:22:33 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,545 @@
+
+// let day = 46;
+
+
+// switch(day){
+//     case 1:
+//         console.log('dushanba');
+// break;
+//         case 2:
+//             console.log('seshanba');
+//             break;
+//             case 3:
+//                 console.log('chorshanba');
+//                 break;
+//                 case 4:
+//                     console.log('payshanba');
+//                     break;
+//                     case 5:
+//                         console.log('juma');
+//                         break;
+//                         case 6:
+//                             console.log('shanba');
+//                             break;
+//                             case 7:
+//                                 console.log(yakshanba);
+//                                 break;
+//                                 default:
+//                                     console.log('not found');
+// }
+
+
+// class Car{
+//     constructor(name,model,weight,speed){
+//         this.name=name;
+//         this.model=model;
+// this.weight=weight;
+// this.speed=speed;
+// }
+
+// getinfo(){
+//     console.log(
+//         this.name+this.model
+//     );
+// }
+
+// }
+
+// let Gentra = new Car("Gentra",3, '1.8 tonna', 200)
+
+
+
+// class Apple{
+//     constructor(name,model,memory,year,width,ios){
+//         this.name=name;
+//         this.model=model;
+//         this.memory=memory;
+//         this.year=year;
+//         this.width=width;
+//         this.ios=ios;
+//     }
+//     getinfo(){
+//         console.log(
+//             this.name+this.model
+//         );
+//     }
+// }
+
+// let apple7 = new Apple("iphone", 7, "64gb",2016,"180gr", 15.8);
+
+
+
+ ///masala
+
+ // N1
+
+// let a = 5;
+
+// switch (jumla) {
+//    case 1:
+//       console.log(jumla);
+//       break;
+//       case 2:
+//          console.log(butun);
+//          break;
+// }
+
+
+// N2
+
+// let a = 6;
+
+// switch (toq) {
+//    case 1:
+//       console.log(jumla);
+//       break;
+//    case 2:
+//       console.log(toq);
+//       break;
+// }
+
+
+// N3
+
+// let a = 9;
+
+// switch (a) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//    case 2:
+//       console.log(juft);
+//       break;
+// }
+
+
+// N4
+
+// let a = 5;
+// let b = 6;
+
+// switch (M-s a > 0 && b > 0) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//       console.log(a < b);
+//       break;
+// }
+
+// N5 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a > b < c) {
+//    case 1:
+//       console.log(a > b);
+//       break;
+//    case 2:   
+//       console.log(b < c);
+//       break;
+//    case 3:
+//       console.log(a > 5);
+//       break;
+// }
+
+
+// N6 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a > b < c) {
+//    case 1:
+//       console.log(a > b);
+//       break;
+//    case 2:   
+//       console.log(b < c);
+//       break;
+//    case 3:
+//       console.log(a > 5);
+//       break;
+// }
+
+
+
+// N7 
+
+// let a = 5;
+// let b = 6;
+
+// switch (a == b) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//       console.log(a < b);
+//       break;
+// }
+
+
+// N8 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+// let d = 10;
+
+// switch (a,b,c.d) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+//    case 4:
+//       console.log(d);
+//       break;
+// }
+
+// N9
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+// N10
+
+// let vaqt = new Date();
+
+// let havta = vaqt.getDate();
+
+// let du = vaqt .getDate();
+// let se = vaqt.getDate();
+// let cho = vaqt .getDate();
+// let pay = vaqt .getDate();
+// let juma = vaqt .getDate();
+// let sha = vaqt .getDate();
+// let yak = vaqt .getDate();
+
+// N11
+
+// let = 110
+
+// switch (110) {
+//    case 1:
+//    console.log(110);
+//    break;
+// }
+
+
+// N12
+
+
+// let = 5;
+
+// switch (5-1) {
+//    case 1:
+//       console.log(5-1);
+//       break;
+// }
+
+// N13
+
+// let = 5;
+
+// switch (5+3-2) {
+//    case 1:
+//       console.log(5+3-2);
+//       break;
+// }
+
+// N14
+
+// let a = 6;
+// let b = 9;
+
+// switch (a > b) {
+//    case value:
+//       console.log(a < b);
+//       break;
+// }
+
+// N15
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < 20 > 2) {
+//    case value:
+//       console.log(b);
+//       break;
+// }
+
+// N16
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < b > c) {
+//    case value:
+//       console.log(c);
+//       break;
+// }
+
+
+
+// N18
+
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < 20 > 2) {
+//    case value:
+//       console.log(b);
+//       break;
+// }
+
+// N19
+
+// let a = 15;
+// let b = 15;
+// switch (true) {
+//     case a % 2 == 0 && b % 2 == 0:
+//         console.log('ikkisi ham juft');
+//         break;
+//     case a % 2 != 0 && b % 2 == 0:
+//         console.log('a soni toq');
+//         break;
+//     case a % 2 == 0 && b % 2 != 0:
+//         console.log('b soni toq');
+//         break;
+//     case a % 2 != 0 && b % 2 != 0:
+//         console.log('ikkisi ham toq');
+//         break;
+//     default:
+//         console.log('bu son emas');
+//         break;
+// }
+
+// N20
+
+// let num = -5;
+
+// switch (true) {
+//     case num > 0:
+//         console.log(++num);
+//         break;
+//     case num < 0:
+//         console.log(++num);
+//         break;
+//     default:
+//         console.log ('son kiriting');
+//         break;
+// }
+
+// N21
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+// N22
+
+// let a =10;
+// let b =30;
+
+// switch (true) {
+//    case 1:
+//       console.log(30 < 10);
+//       break;
+// }
+
+// N23
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+// N24
+
+// let a = 9;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(b);
+//       break;
+//    case 2:   
+//       console.log(c);
+//       break;
+//    case 3:
+//       console.log(a);
+//       break;
+// }
+
+// let gender = 'female'
+
+// switch (gender) {
+//     case 'male':
+//         console.log('erkak');
+//         break;
+//     case 'female':
+//         console.log('ayol');
+//         break
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+
+
+
+// class Mac{
+// constructor(name,model,memory,year){
+//     this.name=name;
+//     this.model=model;
+//     this.memory=memory;
+//     this.year=year;
+// }
+// getinfo(){
+//     console.log(
+//         this.name+this.model
+
+//     );
+// }
+
+// }
+
+// let MacM = new Mac("macbookpro","mac","128gb",'2019');
+
+
+// class MacM1 extends Mac{
+//     constructor(name,model,memory,year,color,faceid){
+//         this.super (name,model,memory,year);
+//         this.color=color;
+//         this.faceid=faceid;
+//     }
+
+// getname(){
+//     console.log(
+//         this.color + this.faceid
+//     );
+// }
+// }
+// let Mac = new MacM1 ('macM1','mac','1TB',2023, 'black' ,'true');
+
+
+
+
+
+
+
+
+class MacM1{
+    constructor(name,model,memory,color,faceid,){
+        this.name=name;
+        this.model=model;
+        this.memory=memory;
+        this.color=color;
+        this.faceid=faceid;
+    }
+    getinfo (){
+        console.log(
+this.name + this.model
+    );
+}
+}
+
+let MacM1 = new Mac('Macm1', 'mac', '1TB', 2023,'true');
+
+
+
+class MacM2 extends MacM1{
+    constructor(name,model,memory,color,faceid,newcolor,newmemory,newcheap){
+        super(name,model,memory,color,faceid);
+        this.newcolor=newcolor;
+        this.newmemory=newmemory;
+        this.newcheap=newcheap;
+    }
+getname(){
+    console.log(
+        this.newcheap + this.newcheap + this.newcolor
+    );
+}
+
+}
+
+let Macm1 = new MacM2('Macm2','mac','1TB','black','true',2024,'gold','2TB','M2');
+
+
+class phone {
+    constructor(name,model,ram,chip,storage){
+        this.name = name;
+        this.model = model;
+        this.ram = ram;
+        this.chip = chip;
+        this.storage = storage
+    }
+    getInfo(){
+        console.log(this.name + ' ' + this.model + ' ' + this.chip) ;
+    }    
+    
+}
+
+
+class more extends phone{
+    constructor(name,model,ram,chip,storage,camera,battery){
+        super(name,model,ram,chip,storage);
+        this.camera=camera;
+        this.battery=battery;
+    }
+    getCamera(){
+        console.log(
+            this.camera + ' ' + this.battery
+        );
+    }
+}
+let iphone = new more('Iphone','XS',3,'S22','512GB','48MP','4852 mAh');
